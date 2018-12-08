0a7333bbdd1d8a6bc48a3b303681c9d7f45d92fe
connor <14878231+yassck02@users.noreply.github.com>
2018-12-08 14:00:47 -0600

    Added new icons + small updates

--- src/src/app/services/experience.service.ts
+++ src/src/app/services/experience.service.ts
@@ -25,7 +25,7 @@ export const EXPERIENCE: IExperience[] = [
         title: 'Software engineering co-op',
         company: 'Rockwell Automation',
         location: 'Milwaukee WI',
-        timeframe: 'May \"18 - Dec \"18',
+        timeframe: 'May \'18 - Dec \'18',
         skills: [
             'Automated Testing',
             'c++ front end developement'
@@ -35,7 +35,7 @@ export const EXPERIENCE: IExperience[] = [
         title: 'Programming intern',
         company: 'Interactive Degree Planner LLC',
         location: 'Whitewater WI',
-        timeframe: 'May \"18 - Dec \"18',
+        timeframe: 'May \'18 - Dec \'18',
         skills: [
             'Designed and implemented algorithms'
         ]
@@ -44,7 +44,7 @@ export const EXPERIENCE: IExperience[] = [
         title: 'Game developement intern',
         company: 'Mobile Mesh Games',
         location: 'Whitewarer WI',
-        timeframe: 'May \"18 - Dec \"18',
+        timeframe: 'May \'18 - Dec \'18',
         skills: [
             'Mobile app developement',
             'IMplamented art assets'
