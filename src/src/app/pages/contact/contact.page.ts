90ae95b268b9c003e795e88f68af3dca3b7438e9
connor <14878231+yassck02@users.noreply.github.com>
2018-12-11 20:47:51 -0600

    Fixed contact and resume page

--- src/src/app/pages/contact/contact.page.ts
+++ src/src/app/pages/contact/contact.page.ts
@@ -6,26 +6,9 @@ import { Component } from '@angular/core';
 })
 export class ContactPage {
     name: string;
-    email: string;
     message: string;
 
     constructor() {}
 
     ngOnInit() {}
-
-    processForm() {
-        if(this.name == undefined || this.name.length < 3) {
-            alert("ERROR: Name must be at least 3 characters")
-            return;
-        }
-        if(this.email == undefined || this.email.length < 3) {
-            alert("ERROR: Please enter a valid email address")
-            return;
-        }
-        if(this.message == undefined || this.message.length <= 0) {
-            alert("ERROR: Please enter a message")
-            return;
-        }
-
-    }
 }
