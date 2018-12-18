f6765a16f8cb51ba49c9aed9023d66581dc4d406
connor <14878231+yassck02@users.noreply.github.com>
2018-12-17 21:39:55 -0600

    Added back button to project detail page

--- src/src/app/pages/projects/project-detail.page.ts
+++ src/src/app/pages/projects/project-detail.page.ts
@@ -5,15 +5,37 @@ import { ProjectService } from '../../services/project.service'
 
 @Component({
     template: `
+        <a id="btn-back" routerLink="/projects">< Back</a>
+        <a id="repo-link" href="{{repo_link}}">Repository</a>
         <div *ngIf="readme === undefined">
             <p>Loading...</p>
         </div>
         <div *ngIf="readme !== undefined">
             <div [innerHTML]="readme"></div>
-            <a href="{{repo_link}}">repository</a>
         </div>
     `,
-    styleUrls: []
+    styles: [`
+        #repo-link { 
+            float: right;
+            display: inline-block;
+            color: gray;
+            text-decoration: underline;
+        }
+        #repo-link:hover { 
+            color: hsl(var(--hue_1), 100%, 50%);
+        }
+        #btn-back {
+            display: inline-block;
+            text-decoration: none;
+            color: hsl(var(--hue_0), 100%, 50%);
+            border: solid 1px white;
+            border-radius: var(--radius);
+            padding: 2px;
+        }
+        #btn-back:hover {
+            border: solid 1px hsl(var(--hue_0), 100%, 50%);
+        }
+    `]
 })
 export class ProjectDetailPage implements OnInit {
 
