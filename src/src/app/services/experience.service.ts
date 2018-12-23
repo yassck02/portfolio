e62f485388eef6ebdb99a6caa8b4a351cfb40039
connor <14878231+yassck02@users.noreply.github.com>
2018-12-22 23:16:52 -0600

    Updated experience items

--- src/src/app/services/experience.service.ts
+++ src/src/app/services/experience.service.ts
@@ -27,8 +27,9 @@ export const EXPERIENCE: IExperience[] = [
         location: 'Milwaukee WI',
         timeframe: 'May \'18 - Dec \'18',
         skills: [
-            'Automated Testing',
-            'c++ front end developement'
+            'Developed and documented user facing features in Python, C#, and C++',
+            'Participated in the full development lifecycle using Scaled Agile methodologies',
+            'Designed and implamented automated test workflows to test pre production code'
         ]
     },
     {
@@ -37,7 +38,10 @@ export const EXPERIENCE: IExperience[] = [
         location: 'Whitewater WI',
         timeframe: 'May \'18 - Dec \'18',
         skills: [
-            'Designed and implemented algorithms'
+            'Designed, implemented, and tested complex algorithms in C++',
+            'Worked with object orient design principles including multiple inheritance, encapsulation, and polymorphism',
+            'Optimized and updated old code to comply with new specifications',
+            'Participated in weekly team meetings to present progress'
         ]
     },
     {
@@ -46,8 +50,9 @@ export const EXPERIENCE: IExperience[] = [
         location: 'Whitewater WI',
         timeframe: 'May \'18 - Dec \'18',
         skills: [
-            'Mobile app developement',
-            'Implamented art assets'
+            'Developed, tested, and debugged C# code for mobile apps',
+            'Programmed menu navigation and control logic for multiple iPhone and Android games',
+            'Consulted with graphic design interns to design and implement user interface elements'
         ]
     },
     {
@@ -56,8 +61,11 @@ export const EXPERIENCE: IExperience[] = [
         location: 'Whitewater WI',
         timeframe: 'Sept \'16 - May \'18',
         skills: [
-            'Operated and maintained a 3D printer and Laser cutter',
-            'Taught students and professors how to use them'
+            'Cleaned, maintained, and operated 3D printer, laser cutter, and other machinery', 
+            'Taught and consulted dozens of students and professors how to use the equipment', 
+            'Consulted students on the manufacturing process of their projects', 
+            'Wrote Javascript plugins for Adobe Illustrator to design fractal patterns for g-code generation', 
+            'Designed 3D printable models for students and professors using Autodesk 3D Studio Max and Maya software packages'
         ]
     }
 ]
