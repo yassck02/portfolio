8d4322221927a198bb49017aed47f4dca4f43624
Yassck02 <14878231+yassck02@users.noreply.github.com>
2019-10-02 20:32:51 -0400

    Updated resume

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -17,36 +17,9 @@ export class AppComponent implements OnInit {
       { index: 4, path: '/resume',     label: 'Résumé'     },
   ];
 
-  currentTab: number = 0;
-
-  onLeftClicked() {
-      this.currentTab = (this.currentTab-1 >= 0) ? this.currentTab-1 : this.tabs.length-1;
-      this.router.navigate([this.tabs[this.currentTab].path]);
-  }
-
-  onRightClicked() {
-      this.currentTab = (this.currentTab+1 < this.tabs.length) ? this.currentTab+1 : 0;
-      this.router.navigate([this.tabs[this.currentTab].path]);
-  }
-
-  onKeyPressed(event: KeyboardEvent) {
-      if(event.key == "ArrowLeft") {
-          this.onLeftClicked();
-      } else if(event.key == "ArrowRight") {
-          this.onRightClicked();
-          this.renderer.setStyle(this.element.nativeElement, 'color', 'red');
-          this.renderer.setStyle(this.element.nativeElement, '--hue_1', '50');
-          console.log("DFEEFEFEFEF");
-      }
-  }
-
   constructor(private router: Router, private element: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-      //document.documentElement.style.setProperty('--hue-0', '50');
-      //document.documentElement.style.setProperty('--hue-1', '50');
-
-      //this.renderer.setStyle(this.element.nativeElement, 'color', 'red');
       this.renderer.setProperty(this.element.nativeElement, 'hue_1', '100');
   }
 }
