3653c4ef87ab9f02593e7d95846fbdd2b96e7c13
Yassck02 <14878231+yassck02@users.noreply.github.com>
2019-11-19 20:17:23 -0500

    added gradient background stuff

---
+++ project/src/app/pages/coverletter/coverletter.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coverletter',
+  templateUrl: './coverletter.page.html',
+  styleUrls: ['./coverletter.page.css']
+})
+export class CoverletterPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
